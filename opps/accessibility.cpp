30ff1577170c0f35e7d78596c2cde82bc3834567
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-07 09:13:22 +0530

    three commit in all ready completead

---
+++ opps/accessibility.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+    // private:
+    // int a;
+    // protected:
+    // int b;
+    public:
+    int c;
+    
+};
+
+int main()
+{
+    A obj;
+    // obj.a=10;
+    // obj.b=20;   
+    obj.c=30;
+
+}

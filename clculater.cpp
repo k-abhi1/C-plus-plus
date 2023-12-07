639e82002a29178ab610a73afe79c3fb43d7af8b
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-07 12:02:48 +0530

    three

---
+++ clculater.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,choice;
+
+    cout<<"enter the first number";
+    cin>>a;
+    cout<<"enter the second number";
+    cin>>b;
+ 
+    cout<<"enter the choice";
+    cin>>choice;
+
+    switch(choice)
+    {
+
+        case 1 :
+        c=a+b;
+        cout<<c<<endl;
+        break;
+
+ case 2 :
+        c=a-b;
+        cout<<c<<endl;
+        break;
+
+ case  3:
+        c=a*b;
+        cout<<c<<endl;
+        break;
+
+ case 4 :
+        c=a/b;
+        cout<<c<<endl;
+        break; 
+
+
+
+        default:cout<<"inviled stement plase enter the key ";
+
+
+
+
+    }
+}

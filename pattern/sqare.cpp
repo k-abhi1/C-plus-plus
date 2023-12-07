1d3e3beb985aadd57ab9cb10864f8156c67b556e
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-08 01:53:54 +0530

    pattern

---
+++ pattern/sqare.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, colum;
+    cout << "enter the row";
+    cin >> row;
+
+    cout << "enter the colum";
+    cin >> colum;
+      
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 1; j < colum; j++)
+        {
+            cout << "*";
+        }  
+        cout << endl;
+    }
+    return 0;
+}

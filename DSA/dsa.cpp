a57f8a75cffd5182a5175748aad43e1c836fd3e4
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-09 10:45:56 +0530

    c

---
+++ DSA/dsa.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "<------------------------------------------------------------------------------->" << endl;
+    cout << "//-------------------------------D.S.A-------------------------------------//" << endl;
+    cout << "<------------------------------------------------------------------------------->" << endl<<endl;
+
+    cout << "-->Introduction to Data Structures:" << endl<<endl;
+    cout << ">What is Data Structure: Types, Classifications and Applications" << endl;
+    cout << ">Introduction to Data Structures" << endl;
+    cout << ">Common operations on various Data Structures" << endl
+         << endl ;
+    cout << "-->Popular types of Data Structures:" << endl<<endl;
+    cout << "1.Array" << endl;
+    cout << "2.Link List" << endl;
+    cout << "3.Stack" << endl;
+    cout << "4.Queue" << endl;
+    cout << "5.Binary Tree" << endl;                //TODO D S A  FIX ME TIME COMLEXCITY
+    cout << "6.Binary Search Tree" << endl;
+    cout << "7.Heap" << endl;
+    cout << "8.Hashing" << endl;
+    cout << "9.Graph" << endl;
+    cout << "10.Matrix" << endl;
+    cout << "11.Misc" << endl;
+    cout << "12.Advanced Data Structure" << endl<<endl;
+
+    cout<<"-->Overview:"<<endl<<endl;
+    cout<<">Introduction to Linear Data Structures"<<endl;
+    cout<<">Introduction to Hierarchical Data Structure"<<endl;
+    cout<<">Overview of Data Structures | Set 3 (Graph, Trie, Segment Tree and Suffix Tree)"<<endl<<endl;
+    cout<<"Abstract Data Types"<<endl<<endl;
+  
+    return 0;
+
+}

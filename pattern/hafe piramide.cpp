bf1b34e6f0aefdb6b8ebd52b82f151fa9114fc03
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-11 11:02:01 +0530

    up date

---
+++ pattern/hafe piramide.cpp
@@ -0,0 +1,62 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int row;
+
+//     cout<<"enter the row ";
+//     cin>>row;
+
+//     for(int i=1; i<=row; ++i)
+//     {
+//         for(int j=1; j<=i; ++j)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int row;
+
+//     cout<<"enter the row ";
+//     cin>>row;
+
+//     for(int i=row; i=>i; --i)
+//     {
+//         for(int j=1; j<=i; ++j)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
--- pattern/sqare.cpp
+++ pattern/sqare.cpp
@@ -1,22 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int row, colum;
-    cout << "enter the row";
-    cin >> row;
+    int row,colum;
 
-    cout << "enter the colum";
-    cin >> colum;
-      
-    for (int i = 1; i <= row; i++)
+    cout<<"Enter the number of rows: ";
+    cin>>row;
+
+    cout<<"Enter the number of colums: ";
+    cin>>colum;
+
+    for(int i=1;i<=row;i++)
     {
-        for (int j = 1; j <=  colum; j++)
+        for(int j=1;j<=colum;j++)
         {
-            cout << "*";
-        }  
-        cout << endl;
+            cout<<"*";
+        }
+        cout<<endl;
     }
     return 0;
 }

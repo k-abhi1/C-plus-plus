0d529e295e9dc75dbca3f2a2303c4b557e5b6118
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-13 11:07:02 +0530

    up

---
+++ c++/type.csting.cpp
@@ -0,0 +1,114 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     // char x='a';
+//     // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='b';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='c';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='d';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='e';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='f';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='g';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='h';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='i';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='j';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+    // char x='k';
+    // int y=x;
+    // cout<<y<<endl<<x<<endl;
+
+   // }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     for (char x = 'a'; x <= 'k'; x++) {
+//         int y = x;
+//         cout << y << endl << x << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include <iostream> 
+// using namespace std; 
+  
+// int main() 
+// { 
+//     float x;
+
+//     cout<<"Enter a number: ";
+//     cin>>x;
+
+//     // Explicit conversion from double to int 
+//     float sum = (float)x + 1; 
+  
+//     cout << "Sum = " << sum<<endl; 
+  
+//     return 0; 
+// } 
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char a='a';
+    int b=a;
+    int input,sum;
+    cout<<"Enter a number: ";
+    cin>>input;
+
+    sum=input+b;
+    cout<<"Sum = "<<b<<endl<<input<<endl<<sum<<endl;
+
+
+}
+  
+
+
+
+  
+
+
+
+
+
+
+    
+    
+
+

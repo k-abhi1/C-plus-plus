1fdbaf57f64b870d0c6fff8111914c734d68d723
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-15 00:11:03 +0530

    up

---
+++ c++/class/2st.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+
+    cout<<"enter the first number";
+    cin>>a;
+
+    cout<<"enter the second number";
+    cin>>b;
+
+    sum=a+b;
+
+    cout<<"the sum is "<<sum;
+
+}
+// Output:
+// enter the first number10
+// enter the second number20
+// the sum is 30
+//
+// Explanation:
+// The above program is a simple program to add two numbers.
+// The program starts with the main() function.
+// The main() function is the entry point of every C++ program.
+// The cout statement is used to print the text "enter the first number".
+
+// The cin statement is used to take input from the user.
+// The sum of the two numbers is stored in the variable sum.
+// The cout statement is used to print the text "the sum is".
+// The sum is printed using the variable sum.
+// The return 0 statement is used to exit the program.

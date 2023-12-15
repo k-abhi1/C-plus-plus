1cbe35b3f083400cd6c1a1c3867e807710820142
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2023-12-15 15:02:00 +0530

    up date

---
+++ c++/class.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+class a
+{
+    public:
+    void show()
+    {
+        cout<<"hello world";
+
+    }
+
+};
+int main()
+{
+    a obj;
+    obj.show();
+
+    return 0;
+
+}

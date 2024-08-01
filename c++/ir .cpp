2f49e9316a1c6c70d8c5cc9ffa1ffe3677230197
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-08-01 19:37:46 +0530

    first

---
+++ c++/ir .cpp
@@ -0,0 +1,51 @@
+#include<IRremote.h>
+
+int RECV_PIN = 11;
+int s;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+void setup() {
+    Serial.begin(9600);
+    irrecv.enableIRIn();
+    Serial.println("Enabled IR in");
+     // Start the receiver
+    }
+void loop(){
+    if(irrecv.decode(&results))
+    {
+        S=results.value,HEX;
+        Serial.println(s);
+        irrecv.resume();
+    }
+    delay(10);
+}
+
+
+
+
+second code 
+
+
+const int pirPin = 9;
+const int motorPin = 2;
+int switchState = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(motorPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  switchState = digitalRead(pirPin);
+  
+  if (switchState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+  }
+}
